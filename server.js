eae025b099a25a679c21c5682e28129646b2b6a0
Cameron Taljaar <ctaljaar@c3r5s6.wethinkcode.co.za>
2019-11-25 07:40:13 +0200

    Commiting a few minor setup changes.

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
-var http = require('http'); // Import Node.js core module
+const express = require("express");
 
-var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
-        
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        
-        // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-    
-    }
-    else if (req.url == "/student") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
-        res.end();
-    
-    }
-    else if (req.url == "/admin") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
-    
-    }
-    else
-        res.end('Invalid Request!');
+const PORT = 8080;
+const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello world");
 });
 
-server.listen(5000); //6 - listen for any incoming requests
+app.get("/goodbye", (req, res) => {
+    res.send("Goodbye world");
+});
 
-console.log('Node.js web server at port 5000 is running..')
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
