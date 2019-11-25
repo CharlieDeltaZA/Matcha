35bcbce07ae2ef63c4d2e12f2b559434b42b96b4
Cameron Taljaar <ctaljaar@c3r5s6.wethinkcode.co.za>
2019-11-25 08:06:09 +0200

    Added index redirect

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
-
 const PORT = 8080;
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.render('index', {
+        title:'Articles',
+    });
 });
 
 app.get("/goodbye", (req, res) => {
     res.send("Goodbye world");
+});
+
+app.get('*', function(req, res) {
+    res.render('404', {
+        title:'404',
+    });
 });
 
 app.listen(PORT, () => {
