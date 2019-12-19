b6c57cef7b50b9d666ab991c4c498682691b9054
Calvin Diogo <calvin@diogo.co.za>
2019-12-19 14:57:29 +0200

    Update register.js

--- scripts/register.js
+++ scripts/register.js
@@ -55,15 +55,6 @@ function printError(msg) {
     err.innerHTML = msg;
 }
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
-
 function checkLogin(uname) {
     //check for username already existing
 }
