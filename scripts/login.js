a9d1b2d5ab8e3044968c1b9e90507cb126e43310
Benjamin WEBB <bwebb@student.wethinkcode.co.za>
2020-01-07 13:40:39 +0200

    input validation of login and registeration

--- scripts/login.js
+++ scripts/login.js
@@ -7,9 +7,6 @@ function LogInCheck() {
     const uname = document.getElementById("userLogin").value;
     const pw = document.getElementById("userPass").value;
 
-    if (checkContent(uname, pw) == 0)
-        return;
-
     // let response = fetch(url);
     // if (response.ok) {
     //     console.log("Good Response");
@@ -28,22 +25,3 @@ function LogInCheck() {
 	xhr.send(JSON.stringify(userDetails));
 	window.location.replace("http://localhost:8080");
 };
-
-function printError(msg) {
-    $("#error").text(msg);
-}
-
-function checkContent(uname, pw) {
-    if ((uname.length == 0 || uname == undefined) && (pw.length == 0 || pw == undefined)) {
-        printError("Please enter your details!");
-        return 0;
-    }
-    if (uname.length == 0 || uname == undefined) {
-        printError("Please enter a username!");
-        return 0;
-    }
-    if (pw.length == 0 || pw == undefined) {
-        printError("Please enter your password!");
-        return 0;
-    }
-}
