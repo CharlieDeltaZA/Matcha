6fe42ecc2f5fd676587e92399a4b51cf3f1a3690
Calvin Diogo <calvin@diogo.co.za>
2020-01-16 10:17:32 +0200

    More chat prep

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -8,30 +8,28 @@ app.use(express.static('/../../styles'));
 app.use(express.static('/../../images'));
 app.use(express.static('/../../scripts'));
 
-const mongoose = require("mongoose");
-const url = "mongodb+srv://admin:admin@cluster0-u7zcd.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-	console.log('connected', err);
+// const mongoose = require("mongoose");
+// const url = "mongodb+srv://admin:admin@cluster0-u7zcd.mongodb.net/test?retryWrites=true&w=majority";
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+// 	console.log('connected', err);
+// });
+
+// var Message = mongoose.model('Message',{ name : String, message : String});
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
+router.get('/', (req, res) => {
+	if (req.session.user == undefined)
+	{
+		res.redirect('/user/login');
+		return ;
+	}
+	res.render('chat', {
+		title:'Chat Messages',
+		user: (req.session.user === undefined ? "Username" : req.session.user),
+		userLogged: (req.session.user === undefined ? false : true)
+	});
 });
-
-var Message = mongoose.model('Message',{ name : String, message : String});
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.get('/messages', (req, res) => {
-		Message.find({},(err, messages)=> {
-			res.send(messages);
-		});
-	});
-
-app.post('/messages', (req, res) => {
-		var message = new Message(req.body);
-		message.save((err) =>{
-			if(err)
-			sendStatus(500);
-			res.sendStatus(200);
-		});
-	});
 
 module.exports = router;
