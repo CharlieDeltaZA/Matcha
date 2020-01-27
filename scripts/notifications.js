98c1e5abcbb3eb1c4469af58c5c202d5742b73ba
Jonathan <jonathanlimbada@gmail.com>
2020-01-27 15:38:15 +0200

    did the thing

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -13,9 +13,6 @@ $(document).ready(function(){
 		$.ajax({
 			type: "POST",
 			url: '/user/notifications',
-			// data: JSON.stringify(send),
-			// contentType: "application/json; charset=utf-8",
-			// dataType: "json",
 			success: function(data) {
 				notifications.likes = data.likes;
 				notifications.messages = data.messages;
@@ -23,9 +20,6 @@ $(document).ready(function(){
 
 				if (notifications.likes > oldNotifications.likes)
 				{
-					// $('.toast').toast({
-					// 	autohide: false
-					// });
 					$('#Toasty').html(`<div class="toast-header"><img class="rounded mr-2" src="/logo.png" alt="Logo" style="width:25px;"/><strong class="mr-auto">Matcha</strong><small class="text-muted">Now</small><button class="ml-2 mb-1 close" type="button" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">×</span></button></div>
 					<div
 						class="toast-body">Someone liked your profile, go to the likes page to dismiss this popup</div>
@@ -34,9 +28,6 @@ $(document).ready(function(){
 				}
 				if (notifications.views > oldNotifications.views)
 				{
-					// $('.toast').toast({
-					// 	autohide: false
-					// });
 					$('#Toasty').html(`<div class="toast-header"><img class="rounded mr-2" src="/logo.png" alt="Logo" style="width:25px;"/><strong class="mr-auto">Matcha</strong><small class="text-muted">Now</small><button class="ml-2 mb-1 close" type="button" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">×</span></button></div>
 					<div
 						class="toast-body">Someone viewed your profile. Go to views page to dismiss this popup</div>
@@ -45,9 +36,6 @@ $(document).ready(function(){
 				}
 				if (notifications.messages > oldNotifications.messages)
 				{
-					// $('.toast').toast({
-					// 	autohide: false
-					// });
 					$('#Toasty').html(`<div class="toast-header"><img class="rounded mr-2" src="/logo.png" alt="Logo" style="width:25px;"/><strong class="mr-auto">Matcha</strong><small class="text-muted">Now</small><button class="ml-2 mb-1 close" type="button" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">×</span></button></div>
 					<div
 						class="toast-body">Someone sent you a message. Go to the chat page to dismiss this popup.</div>
@@ -59,11 +47,9 @@ $(document).ready(function(){
 				document.getElementById("NotifL").innerHTML = data.likes;
 				document.getElementById("NotifM").innerHTML = data.messages;
 				document.getElementById("NotifV").innerHTML = data.views;
-				// document.getElementById('NotifDiv').innerHTML = data;
 			},
 			complete: function() {
 				setTimeout(worker, 5000);
-				// console.log('Complete');
 			}
 		});
 	})();
