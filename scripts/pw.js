7f867c1069bfb5370f90d7f1136edb917dfed3d5
Benjamin WEBB <bwebb@student.wethinkcode.co.za>
2020-01-27 15:36:40 +0200

    added input validation for reset password

--- scripts/pw.js
+++ scripts/pw.js
@@ -73,19 +73,28 @@ function forgotPassHandler(data) {
 }
 
 function resetPass(code) {
+
 	let form = {
 		userCode: code,
 		newPassword: document.getElementById('userPassword').value
 	}
-	$.ajax({
-		type: "POST", 
-		url : '/user/pass_change',
-		data: JSON.stringify(form),
-		contentType: "application/json; charset=utf-8",
-		dataType: "json",
-		success: function(ret) {
-			document.location.href = ("http://localhost:8080/changed");
-		}
+	checkPass(form.newPassword).then(() => {
+		$.ajax({
+			type: "POST", 
+			url : '/user/pass_change',
+			data: JSON.stringify(form),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(ret) {
+				document.location.href = ("http://localhost:8080/changed");
+			}
+		})
+	}, (err) => {
+		swal(
+            'Error!',
+            `${err}`,
+            'error'
+        )
 	})
 }
 
@@ -124,6 +133,12 @@ $("#userEmail").keyup(function(event) {
     }
 });
 
+$("#userPassword").keyup(function(event) {
+    if (event.keyCode === 13) {
+        resetPass($('#userPassword').val());
+    }
+});
+
 function clientEmailValid(email) {
 	let emailTest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if (email === undefined || email == "")
@@ -152,3 +167,35 @@ $("input[type='email']").on("keyup", function(){
 			console.log('invalid email error');
 	};
 });
+
+$("input[type='text']").on("keyup", function(){
+	$(".errorMsg").text("");
+	$("#submit").prop("disabled", true);
+	checkPass($("#userPassword").val()).then((ret) => {
+		$("#submit").prop("disabled", false);
+	}).catch((err) => {
+		$('#passError').text(err);
+	});
+});
+
+function checkPass(password/*, confpassword*/) {
+	return new Promise ( (resolve, reject) => {
+		if (password === undefined || password == "")
+			reject ('Password empty.');
+		if (password.length < 8)
+			reject ('Password too short');
+		if ((/[^A-Za-z0-9]+/.test(password)))
+			reject ('Password contains something other than numbers and letters');
+		if (!(/.*[1-9].*/.test(password)))
+			reject ('Password does not contain numbers');
+		if (!(/.*[a-zA-Z].*/.test(password)))
+			reject ('Password does not contain letters');
+		// if (!(/^[a-zA-Z0-9]*$/.test(password)))
+		// 		return ('Password contains symbols or emojis');
+		// if (confpassword === undefined || confpassword == "")
+		// 	reject ('Confirm Password empty');
+		// if (password != confpassword)
+		// 	reject ('Password mismatch');
+		resolve ('Valid');
+	});
+}
